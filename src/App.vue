5c35e195a12059b3ff960b1e295a89857076ad71
medeiros-yasmin <medeiros_yasmin@outlook.com>
2022-11-22 17:00:32 -0300

    inclusao do vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,127 +1,32 @@
 <template>
-  <v-app>
-
-    <v-app-bar color="purple" hide-on-scroll accent-4 dark shaped app >
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Sistema Colaborativo</v-toolbar-title>
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <template v-slot:extension>
-        <v-tabs class="text-center" align-with-title>
-          <v-tab to="/podcasts">Podcasts</v-tab>
-          <v-tab>Sites</v-tab>
-          <v-tab>Vídeos</v-tab>
-          <v-tab>Artigos</v-tab>
-          <v-tab>Livros</v-tab>
-        </v-tabs>
-      </template>
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-      color="purple"
-    >
-      <v-list
-        nav
-        dense
-        
-      >
-        <v-list-item-group 
-          v-model="group"
-          active-class="cyan--text text--accent-4"
-        >
-          <v-list-item class="white--text" >
-            <v-list-item-icon>
-              <v-icon color="white">mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Início</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item class="white--text">
-            <v-list-item-icon>
-              <v-icon color="white">mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Minha Conta</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
-    
-
-    <v-main>
-      <v-container>
-        <v-row>
-          <v-col
-            v-for="podcast in podcasts"
-            :key="podcast.id"
-            cols="4"
-          >
-          
-            <v-card flat  class="text-xs-center" height="200">{{podcast.titulo}}</v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
-<script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-import { db } from '../src/firebase/firebase-config'
-import { collection, getDocs} from 'firebase/firestore'
+nav {
+  padding: 30px;
 
-export default {
-  name: 'App',
-  //setup(){
-  mounted(){
-   this.podcasts = this.recuperarDocumentos(this.colRef)
-  },
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-  
-  //},
-
-  components: {
-   
-  },
-
-  data: () => ({
-    drawer: false,
-    group: null,
-    podcasts: null,
-    colRef: collection(db, 'publicacao'),
-    
-  }),
-
-  methods: {
-    recuperarDocumentos(colRef){
-     getDocs(colRef)
-      .then(snapshot => {
-        console.log(snapshot.docs)
-        let podcasts = []
-        snapshot.docs.forEach(doc => {
-          podcasts.push({ ...doc.data(), id: doc.id })
-     })
-     console.log('Tem podcast: ', podcasts)
-     this.podcasts = podcasts
-
-     console.log('Tem podcast dentro da variável local: ', this.podcasts)
-   })
-   .catch(err =>{
-     console.log('Retornou erro:' , err.message)
-   })
-   
-    console.log('Chamou a função, que retornou: ', this.podcasts)
-    return this.podcasts
+    &.router-link-exact-active {
+      color: #42b983;
     }
-
-    }
-};
-</script>
+  }
+}
+</style>
