d3815aebe8d6e368bc7d944442f7dbbc4ccf09da
medeiros-yasmin <medeiros_yasmin@outlook.com>
2023-06-27 20:27:20 -0300

    alerta e validacao ao criar publicacao

--- src/views/CriarPublicacaoView.vue
+++ src/views/CriarPublicacaoView.vue
@@ -4,39 +4,44 @@
         <v-container>
             <v-row>
                 <v-col cols="12">
+                    <v-alert :value="alertaErro" dismissible @input="dismissAlert" type="error">
+                        Preencha os campos adequadamente para criar uma publicação
+                    </v-alert>
                     <v-btn @click="$router.go(-1)" style="margin-top:18px" color="white" variant="text" class="white--text"
                         text>
 
                         <v-icon style="margin-right:5px" start>mdi-arrow-left</v-icon> Voltar
                     </v-btn>
+                    
 
 
                     <v-card style="margin-top:18px" color="#5C3C6C" class="mx-auto white--text" height="850" width="1200">
                         <v-card-text class="text-h5 white--text" style="word-spacing: 50px;"> NOVA PUBLICAÇÃO </v-card-text>
 
 
-                        <v-form ref="form" lazy-validation>
+                        <v-form ref="form" v-model="validacao" lazy-validation>
                             <v-col cols="28" sm="14" md="7">
-                                <v-text-field type="text" v-model="titulo" class="custom-label-color"
-                                    background-color="#391D41" color="white" dark shaped filled :counter="40"
-                                    label="Título" required></v-text-field>
+                                <v-text-field :rules="regraNome" type="text" v-model="titulo" class="custom-label-color"
+                                    background-color="#391D41" color="white" dark shaped filled :counter="40" label="Título"
+                                    required></v-text-field>
                             </v-col>
 
                             <v-col cols="28" sm="14" md="7">
-                                <v-text-field type="text" v-model="autor" class="custom-label-color"
+                                <v-text-field :rules="regraNome" type="text" v-model="autor" class="custom-label-color"
                                     background-color="#391D41" color="white" dark shaped filled :counter="40"
                                     label="Autor(a)" required></v-text-field>
                             </v-col>
 
                             <v-col cols="28" sm="14" md="7">
-                                <v-text-field v-model="link" class="custom-label-color" background-color="#391D41"
-                                    color="white" light shaped dark filled :counter="40" label="Link da Publicação"
-                                    required></v-text-field>
+                                <v-text-field :rules="regraLink" v-model="link" class="custom-label-color"
+                                    background-color="#391D41" color="white" light shaped dark filled :counter="40"
+                                    label="Link da Publicação" required></v-text-field>
                             </v-col>
 
                             <v-col cols="28" sm="14" md="7">
-                                <v-textarea v-model="descricao" class="custom-textarea-color" background-color="#391D41"
-                                    shaped dark :counter="200" color="white" input-color clearable filled>
+                                <v-textarea :rules="regraDescricao" v-model="descricao" class="custom-textarea-color"
+                                    background-color="#391D41" shaped dark :counter="200" color="white" input-color
+                                    clearable filled>
                                     <template v-slot:label>
                                         <div>
                                             Descrição
@@ -45,8 +50,8 @@
                                 </v-textarea>
                             </v-col>
                             <v-col cols="28" sm="14" md="7">
-                                <v-select class="custom-textarea-color" label="Categoria" dark theme="dark"
-                                    :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']">
+                                <v-select :rules="[v => !!v || 'A categoria é obrigatória']" class="custom-textarea-color"
+                                    label="Categoria" dark theme="dark" :items="categorias" required>
                                 </v-select>
                             </v-col>
 
@@ -59,7 +64,8 @@
                                             cancel
                                         </v-icon>
                                     </v-btn>
-                                    <v-btn rounded color="#91A366" class="white--text " @click="adicionarPublicacao()">
+                                    <v-btn :disabled="!validacao" rounded color="#91A366" class="white--text "
+                                        @click="adicionarPublicacao()">
                                         Publicar
                                         <v-icon size="25px" class="material-symbols-rounded" right dark>
                                             check
@@ -75,7 +81,7 @@
 
 
                     </v-card>
-                    
+
 
                 </v-col>
             </v-row>
@@ -93,53 +99,59 @@ import { db } from '../firebase/firebase-config'
 
 export default {
     name: 'PublicacaoNova',
-    regraNome: [
-        v => !!v || 'O campo autora/ autor é obrigatório',
-        v => (v && v.length <= 20) || 'O nome deve ter menos de 20 caracteres',
-      ],
-
-      regraLink: [
-        v => !!v || 'O link da publicação é obrigatório',
-        v => {
-            const pattern = /^(|http|https):\/\/[^ "]+$/;
-            return pattern.test(v) || 'Invalid link';
-        }
-    ],
-
-    regraEmail: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ],
 
     mounted() {
 
     },
 
-    created(){
-       this.$store.commit('toggleAppBar', false);
-       /* onAuthStateChanged(auth, (user) => {
-            this.currentUser = user;
-       }); */
-  },
+    created() {
+        this.$store.commit('toggleAppBar', false);
+        /* onAuthStateChanged(auth, (user) => {
+             this.currentUser = user;
+        }); */
+    },
 
     methods: {
         adicionarPublicacao() {
-            addDoc(this.colRef, {
-                titulo: this.titulo,
-                autor: this.autor,
-                descricao: this.descricao,
-            })
-                .then(() => {
-                    console.log('Inserção de novos dados finalizada')
+            if (this.$refs.form.validate()) {
+                addDoc(this.colRef, {
+                    titulo: this.titulo,
+                    autor: this.autor,
+                    descricao: this.descricao,
+                    link: this.link
                 })
+                    .then(() => {
+                        console.log('Inserção de novos dados finalizada')
+                    })
+            }
+            else {
+                this.alertaErro = true;
+                this.fecharAvisoAutomaticamente();
+            }
+
+        },
+
+        dismissAlert() {
+            this.alertaErro = false;
+        },
+
+        async fecharAvisoAutomaticamente(){
+            await new Promise((resolve)=>{
+                setTimeout(()=>{
+                    resolve();
+                }, 5000);
+            });
+            this.alertaErro = false;
         },
 
     },
 
     data: () => ({
+        alertaErro: false,
         podcast: null,
         publicacaoId: null,
         publicacaoSelecionada: null,
+        validacao: true,
         docRef: null,
         colRef: collection(db, 'sites'),
         titulo: "",
@@ -148,6 +160,29 @@ export default {
         autor: "",
         categoria: "",
         currentUser: null,
+        categorias: ['Artigo', 'Auxílio', 'Podcast', 'Site', 'Vídeo', 'Livros/ Revistas'],
+        regraNome: [
+            v => !!v || 'O campo autora/ autor é obrigatório',
+            v => (v && v.length <= 20) || 'O nome deve ter menos de 20 caracteres',
+        ],
+
+        regraLink: [
+            v => !!v || 'O link da publicação é obrigatório',
+            v => {
+                const pattern = /^(|http|https):\/\/[^ "]+$/;
+                return pattern.test(v) || 'Link inválido.';
+            }
+        ],
+
+        regraEmail: [
+            v => !!v || 'E-mail is required',
+            v => /.+@.+\..+/.test(v) || 'Insira um e-mail válido.',
+        ],
+
+        regraDescricao: [
+            v => !!v || 'A descrição é obrigatória',
+            v => (v && v.length >= 10) || 'A descrição deve conter ao menos 10 caracteres.',
+        ],
     }),
 }
 
