5747e6ab42f01c19f361fa640efa9dbf504198ae
medeiros-yasmin <medeiros_yasmin@outlook.com>
2023-08-20 14:58:59 -0300

    espaco para dados do usuario

--- src/App.vue
+++ src/App.vue
@@ -46,6 +46,25 @@
         dense
         
       >
+
+      <v-list>
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-content >
+              <v-list-item-title dark class="text-h6 white--text">
+                Sandra Adams
+              </v-list-item-title>
+              <v-list-item-subtitle class="white--text">sandra_a88@gmail.com</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
         <v-list-item-group 
           v-model="group"
           active-class="cyan--text text--accent-4"
@@ -70,13 +89,19 @@
             </v-list-item-icon>
             <v-list-item-title>Minhas Publicações</v-list-item-title>
           </v-list-item>
-
-          <v-list-item @click="sair()" class="white--text">
+          <div class="pa-1">
+          <v-list-item @click="sair()">
+            <v-btn block>
             <v-list-item-icon>
-              <v-icon color="white">mdi-logout</v-icon>
+              
+              <v-icon >mdi-logout</v-icon>
+            
             </v-list-item-icon>
+            
             <v-list-item-title>Sair</v-list-item-title>
+          </v-btn>
           </v-list-item>
+        </div>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
