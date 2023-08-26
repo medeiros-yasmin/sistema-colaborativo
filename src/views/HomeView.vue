1ee4530b93f28c44dee46aad982e8c39b1a27528
medeiros-yasmin <medeiros_yasmin@outlook.com>
2023-08-25 22:00:48 -0300

    mudanca de estilo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,45 +1,47 @@
 <template>
-  <v-app style="background-color:#391D41;">
-      <v-main>
-          <v-container>
-              <v-row>
-                  <v-col v-for="item in items" :key="item.id" cols="4">
+    <v-app style="background-color:#391D41;">
+        <v-main>
+            <v-container>
+                <v-row>
+                    <v-col v-for="item in items" :key="item.id" cols="4">
 
-                      <v-card shaped style="margin-top:18px" color="#5C3C6C" :elevation="item - 10" class="overflow-hidden mx-auto white--text"
-                          height="330" width="1000">
-                          <v-menu bottom left>
-                              <template v-slot:activator="{ on, attrs }">
-                                  <v-list-item class="topright">
-                                      <v-btn variant="outlined" icon v-bind="attrs" v-on="on">
-                                          <v-icon color="white">mdi-dots-vertical</v-icon>
-                                      </v-btn>
-                                  </v-list-item>
-                              </template>
+                        <v-card shaped style="margin-top:18px" :color="item.cardColor" :elevation="item - 10"
+                            @mouseover="item.isHovered = true" @mouseout="item.isHovered = false"
+                            :class=" { 'card-hovered': item.isHovered }" height="330" width="1000" class="overflow-hidden mx-auto white--text card-original-color">
+                            <v-menu bottom left>
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-list-item class="topright">
+                                        <v-btn variant="outlined" icon v-bind="attrs" v-on="on">
+                                            <v-icon color="white">mdi-dots-vertical</v-icon>
+                                        </v-btn>
+                                    </v-list-item>
+                                </template>
 
-                              
-                          </v-menu>
-                          <div class="d-flex flex-no-wrap justify-space-between">
-                              <div style="margin-top:18px; margin-left: 18px; margin-right: 18px">
-                                  <v-card-title class="text-h5" v-text="item.title">
-                                  </v-card-title>
-                                  <v-card-subtitle v-text="item.title"></v-card-subtitle>
-                                  <v-card-text class="overflow-hidden text-justify text-h7 font-weight-bold" v-text="item.title"></v-card-text>
-                              </div>
-                              <v-avatar rounded align="center" class="mt-16 mr-5 " size="170">
-                                <v-icon size="150px" class="material-symbols-rounded" color="#E6E7E9">
-                                                share
-                                            </v-icon>
+
+                            </v-menu>
+                            <div class="d-flex flex-no-wrap justify-space-between">
+                                <div style="margin-top:18px; margin-left: 18px; margin-right: 18px">
+                                    <v-card-title class="text-h5" v-text="item.title">
+                                    </v-card-title>
+                                    <v-card-subtitle v-text="item.title"></v-card-subtitle>
+                                    <v-card-text class="overflow-hidden text-justify text-h7 font-weight-bold"
+                                        v-text="item.title"></v-card-text>
+                                </div>
+                                <v-avatar rounded align="center" class="mt-16 mr-5 " size="170">
+                                    <v-icon size="150px" class="material-symbols-rounded" color="#E6E7E9">
+                                        share
+                                    </v-icon>
                                 </v-avatar>
-                          </div>
-                      </v-card>
+                            </div>
+                        </v-card>
 
-                  </v-col>
-              </v-row>
+                    </v-col>
+                </v-row>
 
 
-          </v-container>
+            </v-container>
 
-          <!-- <v-card-text style="height: 100px;">
+            <!-- <v-card-text style="height: 100px;">
               <v-fab-transition>
                   <BotaoAdicionarPubli>
                       
@@ -47,7 +49,7 @@
               </v-fab-transition>
           </v-card-text> -->
 
-          <!-- <v-dialog v-model="dialog" max-width="500px">
+            <!-- <v-dialog v-model="dialog" max-width="500px">
               <v-card>
                   <v-card-text>
                       <v-text-field label="File name"></v-text-field>
@@ -64,9 +66,9 @@
                   </v-card-actions>
               </v-card>
           </v-dialog> -->
-      </v-main>
+        </v-main>
 
-  </v-app>
+    </v-app>
 </template>
 
 <script>
@@ -74,23 +76,35 @@
 //import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  data: () => ({
-        
+    name: 'HomeView',
+    data: () => ({
+
         items: [
-            { id: 0, title: 'Artigos' },
-            { id: 1, title: 'Auxílios' },
-            { id: 2, title: 'Podcasts' },
-            { id: 3, title: 'Sites' },
-            { id: 4, title: 'Vídeos' },
-            { id: 5, title: 'Livros e Revistas' },
+            { id: 0, title: 'Artigos', isHovered: false },
+            { id: 1, title: 'Auxílios' , isHovered: false },
+            { id: 2, title: 'Podcasts', isHovered: false },
+            { id: 3, title: 'Sites', isHovered: false },
+            { id: 4, title: 'Vídeos', isHovered: false },
+            { id: 5, title: 'Livros e Revistas', isHovered: false },
         ],
 
     }),
 
-    created(){
-       this.$store.commit('toggleAppBar', false);
-  },
+    created() {
+        this.$store.commit('toggleAppBar', false);
+    },
+
 
 }
 </script>
+
+<style scoped>
+
+.card-original-color{
+    background-color: #5C3C6C;
+}
+.card-hovered:hover{
+    background-color: #794A81;
+}
+</style>
+
